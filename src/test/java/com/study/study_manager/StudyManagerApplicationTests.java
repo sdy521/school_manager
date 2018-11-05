452edb0962718998675403b10f9bf0906874efed
sdy <1035528554@qq.com>
2018-11-05 22:17:06 +0800

    框架搭建

---
+++ src/test/java/com/study/study_manager/StudyManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.study_manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
