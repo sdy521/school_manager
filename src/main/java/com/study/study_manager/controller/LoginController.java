f9a722a7137d5332e313685d07281aaffc97e2da
sudeyu <1035528554@qq.com>
2018-11-26 17:33:47 +0800

    完成freemake配置

--- src/main/java/com/study/study_manager/controller/LoginController.java
+++ src/main/java/com/study/study_manager/controller/LoginController.java
@@ -1,15 +1,14 @@
 package com.study.study_manager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class LoginController {
 
     @RequestMapping("/login")
-    public String login(){
-        return "/login";
+    public String login(ModelMap map){
+        return "login";
     }
 }
