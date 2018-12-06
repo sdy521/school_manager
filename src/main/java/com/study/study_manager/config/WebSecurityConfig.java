c6042c702f1e3a53851d5b44277526e4da39cc34
sudeyu <1035528554@qq.com>
2018-12-06 16:45:34 +0800

    写security登录验证（未完成）

--- src/main/java/com/study/study_manager/config/WebSecurityConfig.java
+++ src/main/java/com/study/study_manager/config/WebSecurityConfig.java
@@ -1,20 +1,28 @@
 package com.study.study_manager.config;
 
+import com.study.study_manager.security.LoginSuccessHandler;
 import com.study.study_manager.service.CustomUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import javax.annotation.Resource;
 
 //配置Spring Security
 @Configuration
+@EnableWebSecurity // 注解开启Spring Security的功能
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     UserDetailsService customUserService() {
         return new CustomUserService();
     }
+
+    @Resource
+    private LoginSuccessHandler loginSuccessHandler;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,9 +31,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //禁用csrf
+        http.csrf().disable();
+        //定义哪些url需要保护，哪些url不需要保护
         http.authorizeRequests()
+                //所有的请求都需要认证
                 .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").failureUrl("/login?error").permitAll().and()
-                .logout().permitAll();
+                //定义当需要用户登录时候，转到的登录页面
+                .and().formLogin().loginPage("/login")
+                //登录处理url
+                .loginProcessingUrl("/spring_security_check")
+                //登录时对应的用户名密码参数名
+                .usernameParameter("username").passwordParameter("password").successHandler(loginSuccessHandler);
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //对以下路径忽略过滤
+        web.ignoring().antMatchers("/static/**", "/login");
+    }
+
+   /* @Bean
+    public SavedRequestAwareAuthenticationSuccessHandler loginSuccessHandler() { //登入处理
+        return new SavedRequestAwareAuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                logger.info("USER : " + userDetails.getUsername() + " LOGIN SUCCESS !  ");
+                response.sendRedirect("/index");
+                super.onAuthenticationSuccess(request, response, authentication);
+            }
+        };
+    }*/
+
 }
---
+++ src/main/java/com/study/study_manager/security/LoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.study.study_manager.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component("LoginSuccessHandler")
+public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    // Authentication  封装认证信息
+    // 登录方式不同，Authentication不同
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws ServletException, IOException {
+        logger.info("MyAuthenticationSuccessHandler login success!");
+    }
+}
