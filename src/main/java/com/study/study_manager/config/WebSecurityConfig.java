2b6117dd11ee59c7ccfd670e2282f8aad10ea754
sudeyu <1035528554@qq.com>
2018-12-18 16:45:42 +0800

    security登录安全完成

--- src/main/java/com/study/study_manager/config/WebSecurityConfig.java
+++ src/main/java/com/study/study_manager/config/WebSecurityConfig.java
@@ -1,26 +1,19 @@
 package com.study.study_manager.config;
 
-import com.study.study_manager.security.LoginSuccessHandler;
 import com.study.study_manager.service.MyUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.session.InvalidSessionStrategy;
 
-//配置Spring Security
 @Configuration
-@EnableWebSecurity // 注解开启Spring Security的功能
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -32,8 +25,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //在内存中比较
-//        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("sdy").password(new BCryptPasswordEncoder().encode("123")).roles("USER");
           auth.userDetailsService(userDetailsService()).passwordEncoder(new BCryptPasswordEncoder());
     }
 
@@ -44,8 +35,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin().loginPage("/login")
                 //登录处理url
                 .loginProcessingUrl("/j_spring_security_check")
-                //登录时对应的用户名密码参数名
-//                .usernameParameter("username").passwordParameter("password")
                 .and()
                 .authorizeRequests()
                 .anyRequest()//任何请求登陆后都可以访问
--- src/main/java/com/study/study_manager/controller/LoginController.java
+++ src/main/java/com/study/study_manager/controller/LoginController.java
@@ -1,10 +1,8 @@
 package com.study.study_manager.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
