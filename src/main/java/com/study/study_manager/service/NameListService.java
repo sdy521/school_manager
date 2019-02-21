bd47acbe3c9ef6d72f3a239b5c80f44c620999eb
sudeyu <1035528554@qq.com>
2019-02-21 11:13:58 +0800

    教师导入 增加结果显示

--- src/main/java/com/study/study_manager/service/NameListService.java
+++ src/main/java/com/study/study_manager/service/NameListService.java
@@ -105,12 +105,20 @@ public class NameListService extends BaseService<User> {
             user.setDeleted(false);
             user.setImg(null);
             //插入结果
-            /*Cell createCell = row.createCell(2);
-            createCell.setCellType(Cell.CELL_TYPE_STRING);*/
+            Cell createCell = row.createCell(2);
+            createCell.setCellType(Cell.CELL_TYPE_STRING);
+            createCell.setCellValue("导入成功");
             list.add(user);
         }
         if (!CollectionUtils.isEmpty(list)){
             userDao.insertList(list);
+        }
+        try {
+            book.write(os);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return true;
     }
