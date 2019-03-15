b72f50be2dd4236af6f6c5d5823e9581eee10c30
sudeyu <1035528554@qq.com>
2019-03-15 16:57:45 +0800

    改善 适配linux

--- src/main/java/com/study/study_manager/config/WebMvcConfig.java
+++ src/main/java/com/study/study_manager/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.MultipartConfigElement;
+import java.io.File;
 
 @Configuration
 public class WebMvcConfig extends  WebMvcConfigurerAdapter {
@@ -30,7 +31,7 @@ public class WebMvcConfig extends  WebMvcConfigurerAdapter {
          *  对文件的路径进行配置,创建一个虚拟路径/Path/** ，即只要在<img src="/imgPath/picName.jpg" />便可以直接引用图片
          *这是图片的物理路径  "file:/+本地图片的地址"
          */
-        registry.addResourceHandler("/imgPath/**").addResourceLocations("file:/"+location);
+        registry.addResourceHandler("/imgPath/**").addResourceLocations("file:"+location+ File.separator);
         super.addResourceHandlers(registry);
     }
 
